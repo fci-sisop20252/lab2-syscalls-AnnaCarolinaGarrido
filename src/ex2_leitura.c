b76dff35813b13546c8519d5a9538b8bcf5eed73
Anna Carolina Garrido <83478044+AnnaCarolinaGarrido@users.noreply.github.com>
2025-08-26 23:54:55 +0000

    ex 2 code

--- src/ex2_leitura.c
+++ src/ex2_leitura.c
@@ -19,7 +19,7 @@
 int main() {
     char buffer[BUFFER_SIZE];
     ssize_t bytes_lidos;
-    int fd;
+    int fd, cs;
     
     printf("=== Exercício 2: Leitura de Arquivo ===\n\n");
     
@@ -27,13 +27,14 @@ int main() {
      * TODO 1: Abrir o arquivo 'dados/teste1.txt' para leitura
      * Use open() com O_RDONLY
      */
-    fd = /* COMPLETE AQUI */;
+    fd = open("dados/teste1.txt", O_RDONLY);
     
     /*
      * TODO 2: Verificar se a abertura foi bem-sucedida
      * Se fd < 0, houve erro
      */
-    if (/* COMPLETE AQUI */) {
+
+    if (fd == -1) {
         perror("Erro ao abrir arquivo");
         return 1;
     }
@@ -44,12 +45,13 @@ int main() {
      * TODO 3: Ler dados do arquivo
      * Use read() para ler até (BUFFER_SIZE - 1) bytes
      */
-    bytes_lidos = /* COMPLETE AQUI */;
+
+    bytes_lidos = read(fd, buffer, (BUFFER_SIZE-1)); /* COMPLETE AQUI */;
     
     /*
      * TODO 4: Verificar se a leitura foi bem-sucedida
      */
-    if (/* COMPLETE AQUI */) {
+    if (bytes_lidos == -1) {
         perror("Erro na leitura");
         close(fd);
         return 1;
@@ -59,7 +61,8 @@ int main() {
      * TODO 5: Adicionar terminador nulo
      * Para tratar o buffer como string
      */
-    /* COMPLETE AQUI */;
+
+    buffer[bytes_lidos] = '\0';
     
     // Exibir resultados
     printf("Bytes lidos: %ld\n", bytes_lidos);
@@ -69,7 +72,11 @@ int main() {
      * TODO 6: Fechar o arquivo
      * Use close() para liberar o file descriptor
      */
-    if (/* COMPLETE AQUI */) {
+
+    cs = close(fd);
+
+    
+    if (cs == -1) {
         perror("Erro ao fechar arquivo");
         return 1;
     }
